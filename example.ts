d2314448da2ced49c5543d7dd0b6432118f75513
Lopez Family <lopezfamily@lopezs-air.lan>
2022-07-22 14:51:05 -0400

    Created a shipping function that adds price and shipping to understand the ins and outs of typescript. Its an optional course from the complete Angular developer course from Zero to Mastery Academy

---
+++ example.ts
@@ -0,0 +1,6 @@
+function addShipping(price: number, shipping: number) : number {
+    return price + shipping;
+    
+}
+
+addShipping(3, 5);
---
+++ variables.ts
@@ -0,0 +1,8 @@
+/**
+ * There are every primative type are supported in Typescript:
+ * 
+ * string, number, boolean, null, undefined
+ * 
+ */
+let myName = ''; // variable is inferred to string
+myName = '4';
