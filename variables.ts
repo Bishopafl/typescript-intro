3919c4b62777221832f1d66a3616ea415462d5d9
Lopez Family <lopezfamily@lopezs-air.lan>
2022-07-27 12:49:39 -0400

    classes within typescript and how to construct them

--- variables.ts
+++ variables.ts
@@ -51,4 +51,32 @@ const account: IAccount = {
     balance: 10,
 };
 
-let accounts: IAccount[]; // this tells typescript the variable will store an array of objects
+let accounts: IAccount[]; // this tells typescript the variable will store an array of objects based on the interface
+
+/**
+ * Classes
+ * 
+ * The keyword implements lets typescript know to use the interface created above
+ * 
+ * Constructor method is needed with the account variables created on :50 and :51
+ * 
+ * You can even make methods private such as the withdraw()
+ * This is a good usecase to prevent yourself from accidentally 
+ * calling the wrong method or preventing a library from calling it
+ * 
+ * if public is omitted from the constructor function properties the parameters will not be initalized
+ * 
+ */
+
+class InvestmentAccount implements IAccount {
+    constructor(
+        public name, 
+        public balance,
+    ) {
+
+    }
+    
+    private withdraw() {
+
+    }
+}
